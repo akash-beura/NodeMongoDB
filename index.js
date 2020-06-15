d7ffb67a1217e75b90808f4e6d4b317c2fb548bc
M1049006 <akash.beura@mindtree.com>
2020-06-15 19:12:55 +0530

    Node Mongo Example 2: Modularizing the DB operation code and using it in the index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dbOperation = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbName = 'conFusion';
@@ -10,24 +11,21 @@ mongoClient.connect(url, (err, client)=>{
     console.log('Connected to server..');
     // chosing the db name after the connection is succesful
     const db = client.db(dbName);
-    // Select the collection
-    const collection = db.collection('dishes');
-    // collection. will give you 2-3 methods to insert data
-    // that insert function's 2nd param is a callback
-    collection.insertOne({"name": "Green wave mexican pizza", "description": "Yummy!"}, (err, result)=>{
-        assert.equal(err, null);
-        console.log("Data inserted into collection");
-        console.log(result.ops);
-        collection.find({}).toArray((err, docs)=>{
-            //2nd params docs refers to all the doccuments in the collection
-            assert.equal(err, null);
-            console.log("Found documents");
-            console.log(docs);
-            // method to drop the collection
-            db.dropCollection('dishes', (err, result)=>{
-                assert.equal(err, null);
-                client.close();
-            })
-        })
-    });
-})
+    
+    dbOperation.insertDocument(db, {"name":"abcd", "description":"test"}, "dishes", (result)=>{
+        
+        console.log("Insert document:\n", result.ops);
+
+        dbOperation.findDocuments(db, "dishesh", (docs)=>{
+            console.log("Found documents:\n", docs);
+
+            // update == field which you want to update
+            dbOperation.updateDocument(db, {"name":"abcd"}, {"description":"updated test"}, "dishes", (result)=>{
+                console.log("Updated document: \n", result.result);
+                db.dropCollection('dishes', (err, result)=>{
+                    console.log("Dropped collection dishes: ", result);
+                })
+            });
+        });
+    }); 
+});
